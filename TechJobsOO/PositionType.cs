08a8e5d45be84de3be6b1bcb7ec29af23b61de67
Charles Miller <clm7a5@yahoo.com>
2020-11-04 00:13:20 -0600

    Last updates

--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class PositionType
+    
+    public class PositionType : JobField
     {
-        public int Id { get; }
+        
+        public PositionType(string value) : base(value)
+        {
+        }
+        /*public int Id { get; }
         private static int nextId = 1;
         public string Value { get; set; }
         public PositionType()
@@ -15,30 +20,24 @@ namespace TechJobsOO
         {
             Value = value;
         }
-
         // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
-
         public override string ToString()
         {
             return Value;
         }
-
         public override bool Equals(object obj)
         {
             return obj is PositionType type &&
                    Id == type.Id &&
                    Value == type.Value;
         }
-
         public override int GetHashCode()
         {
             return HashCode.Combine(Id, Value);
-        }
+        
+        }*/
 
-        public static implicit operator PositionType(string v)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 
 }
